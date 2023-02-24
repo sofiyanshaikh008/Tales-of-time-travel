7aaed4389ef2ab7530ab134dd9afd38d6c3e2f40
Tahir Manzoor <tahirmanzoorbhat110@gmail.com>
2023-02-25 00:52:01 +0530

    Feedback added

---
+++ BusReservationSystemPortal/src/main/java/com/masai/Exception/FeedbackException.java
@@ -0,0 +1,13 @@
+package com.masai.Exception;
+
+public class FeedbackException extends Exception{
+	
+	public FeedbackException(){
+		
+	}
+	
+	public FeedbackException(String message){
+		super(message);
+	}
+
+}
